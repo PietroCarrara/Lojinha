58d46c45b6a385afa07489c6396e47320f14a4d2
Pietro Carrara <piticarrara@gmail.com>
2018-07-17 05:28:28 -0300

    Conexão básica

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * A home de um novo começo
+ *
+ * @package index
+ * @author pietro <pietro@the-arch>
+ * @version 0.1
+ * @copyright (C) 2018 pietro <pietro@the-arch>
+ * @license MIT
+ */
+
+// require_once __DIR__ . '/vendor/autoload.php';
+
+$hostname = "127.0.0.1";
+$user = "lojinha";
+$pass = "password";
+$db = "test";
+$conexao = new mysqli($hostname,$user,$pass,$db);
+
+echo $conexao->error;
+echo $conexao->connect_error;
+
+echo '<br><br>';
+
+$result = $conexao->query('SELECT * FROM users');
+
+while ($row = $result->fetch_object()){
+	foreach($row as $k => $v) {
+		echo "$k: $v <br>";
+	}
+}
+
+?>
