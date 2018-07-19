c0a3a0d4476348886ffa8b77bc7ab01da0ae4097
ArthurRosso <arthurderosso@gmail.com>
2018-07-19 02:08:13 +0000

    Merge pq o git não me deixou dar pull

--- src/home.php
+++ src/home.php
@@ -4,152 +4,64 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Lojinha</title>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet" />
+
+	<!--Import materialize.css-->
+	<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css" />
+
 </head>
 <body>
 
-	<!-- Navbar -->
-	<?php
-	require 'navbar.html';
-	?>
-	<!-- End Navbar -->
+	<nav>
+		<div class="nav-wrapper">
+			<a href="#" class="brand-logo">Lojinha</a>
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
+				<li><a href="/sacola">Sacola</a></li>
+				<li><a href="/login">Login</a></li>
+			</ul>
+		</div>
+	</nav>
 
-	<!-- Imagem grande que chama atenção -->
-	<header class="full header valign-wrapper">
-		<div class="container valign">
-			<div class="row">
-				<div class="col s12 m6">
-					<figure>
-						<img src="assets/img/header.png"  alt="Header" class="responsive-img center-box">
-					</figure>
-				</div>
-				<div class="col s12 m6">
-					<h5 class="tittles center-align">Lojinha Doki Doki</h5>
-					<p class="flow-text">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing. 
-					</p>
-				</div>
+
+	<div class="container">
+		<div class="row">
+			<div>
+				<form>
+					<div class="input-field" id="search">
+						<input type="text" id="search" name="search" />
+						<label for="search"><i class="material-icons">search</i></label>
+					</div>
+				</form>
 			</div>
 		</div>
-	</header>
-	<!-- End Imagem grande que chama atenção -->
 
-	<!-- Três cards com alguns produtos de destaque -->
-	<section class="full new-prod">
-		<h5 class="tittles center-align">Lorem ipsum dolor sit amet</h5>
-		<div class="container">
-			<div class="row">
-				<div class="col s12 m6 l4">
-					<div class="card hoverable">
-						<figure class="card-image">
-							<img src="assets/img/header.png" >
-							<figcaption class="card-title">Card Title</figcaption>
-						</figure>
-						<div class="card-content">
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-								tempor incididunt.
-							</p>
-						</div>
-						<div class="card-action">
-							<a href="#!">This is a link</a>
-						</div>
+		<div class="row">
+
+			<!-- Isso é o que vai iterar -->
+			<div class="col s3">
+				<div class="card">
+					<div class="card-image">
+						<img src="produtos/miniatura1.png">
+						<a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 					</div>
-				</div>	
-				<div class="col s12 m6 l4">
-					<div class="card hoverable">
-						<figure class="card-image">
-							<img src="assets/img/header.png" >
-							<figcaption class="card-title">Card Title</figcaption>
-						</figure>
-						<div class="card-content">
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-								tempor incididunt.
-							</p>
-						</div>
-						<div class="card-action">
-							<a href="#!">This is a link</a>
-						</div>
-					</div>
-				</div>
-				<div class="col s12 m6 l4">
-					<div class="card hoverable">
-						<figure class="card-image">
-							<img src="assets/img/header.png" >
-							<figcaption class="card-title">Card Title</figcaption>
-						</figure>
-						<div class="card-content">
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-								tempor incididunt.
-							</p>
-						</div>
-						<div class="card-action">
-							<a href="#!">This is a link</a>
-						</div>
+					<div class="card-content">
+						<span class="card-title">$product_name</span>
+						<p>Olá, eu valho R$ $product_price e se tu apertar no FAB eu iriei pro seu carrinho :) </p>
 					</div>
 				</div>
 			</div>
+			<!-- End of iteração -->
 		</div>
-	</section>
-	<!-- End Três cards com alguns produtos de destaque -->
-
-	<!-- Formas de pagamento para encher linguiça -->
-	<section class="full promo">
-		<div class="container">
-			<div class="row">
-				<div class="col s12">
-					<h5 class="tittles center-align">Formas de Pagamento</h5>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita quos dolore modi officiis eius voluptas consequatur facilis, praesentium aspernatur! Ipsum qui ratione sequi veniam facilis quaerat sit porro consectetur itaque!
-					</p>
-				</div>
-				<div class="col s12 m4">
-					<p class="center-align">
-						<i class="zmdi zmdi-card-giftcard zmdi-hc-5x"></i>
-					</p>
-					<p class="center-align flow-text">Lorem ipsum dolor</p>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.
-					</p>
-				</div>
-				<div class="col s12 m4">
-					<p class="center-align">
-						<i class="zmdi zmdi-card zmdi-hc-5x"></i>
-					</p>
-					<p class="center-align flow-text">Lorem ipsum dolor</p>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.
-					</p>
-				</div>
-				<div class="col s12 m4">
-					<p class="center-align">
-						<i class="zmdi zmdi-money zmdi-hc-5x"></i>
-					</p>
-					<p class="center-align flow-text">Lorem ipsum dolor</p>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.
-					</p>
-				</div>
-			</div>
-		</div>	
-	</section>
-	<!-- Formas de pagamento para encher linguiça -->
-
-	<!-- Footer -->
-	<?php
-	require 'footer.html';
-	?>
-	<!-- End Footer -->
+	</div>
 
 
-	<script src="js/jquery-1.11.2.min.js"></script>
-	<script src="js/materialize.min.js" ></script>
-	<script src="js/main.js" ></script>
+	<!--Import jQuery before materialize.js-->
+	<script type="text/javascript"
+	src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 </body>
 </html>
